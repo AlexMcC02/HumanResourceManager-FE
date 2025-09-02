c855baffc4ef49af6dfd13b081b38124082c8e85
Alex McCullough <alex.mccullough@kainos.com>
2025-09-02 15:24:30 +0100

    feat: view employees returned from the API

--- src/Employees.tsx
+++ src/Employees.tsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
+
 function Employees() {
+  const [employees, setEmployees] = useState([]);
+  const fetchEmployees = async () => {
+    try {
+      const url = "http://localhost:5030/human_resource_manager/api/employees/";
+      const response = await fetch(url);
+      console.log("Attempted to reach: " + url);
+      const data = await response.json();
+      console.log("Fetched employees:" + data);
+      setEmployees(data);
+    } catch (error) {
+      console.error("Error fetching employees:" + error);
+    }
+  };
+
+  useEffect(() => {
+    if (!employees.length) fetchEmployees();
+  });
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-black text-white">
-      <div className="text-center">
+    <div className="flex items-start justify-center min-h-screen bg-cover bg-center bg-black text-white">
+      <div className="text-center mt-3">
         <h1 className="text-5xl font-bold">Human Resource Manager</h1>
         <p className="text-xl text-blue-400">This is the employees page!</p>
+        <div className="overflow-x-auto mt-6">
+          <table className="min-w-full border border-gray-700 text-white">
+            <thead className="bg-gray-800">
+              <tr>
+                <th className="px-4 py-2 border border-gray-700">First Name</th>
+                <th className="px-4 py-2 border border-gray-700">Last Name</th>
+                <th className="px-4 py-2 border border-gray-700">Band</th>
+                <th className="px-4 py-2 border border-gray-700">Job Role</th>
+                <th className="px-4 py-2 border border-gray-700">Salary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee, idx) => (
+                <tr key={idx} className="hover:bg-gray-700">
+                  <td className="px-4 py-2 border border-gray-700">
+                    {employee["firstName"]}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-700">
+                    {employee["secondName"]}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-700">
+                    {employee["band"]}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-700">
+                    {employee["jobRole"]}
+                  </td>
+                  <td className="px-4 py-2 border border-gray-700">
+                    {employee["salary"]}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
